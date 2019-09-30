c23d981f79416e42d688f11da0120eb0d3c5dd7e
dassbrothers <37453124+dassbrothers@users.noreply.github.com>
2019-10-01 02:15:00 +0530

    modified the drop down menu to work

--- demo_script_src.js
+++ demo_script_src.js
@@ -1,45 +1,10 @@
+var global_cnt = 1;
 $(document).ready(function() { 
 	$("#but").click(function() {
-		
-		var selectedText = "";
-		if ($("#vdd").is( 
-		  ":checked")) { 
-			//alert("vdd Check box is Checked"); 
-			selectedText = selectedText + "vdd" + " " + $("#vddArtefact").val() + "," ;
-			//alert($("#vddArtefact").val());
-		
-		} else { 
-			//alert("vdd Check box is Unchecked"); 
-		}
-		
-		if ($("#vout").is( 
-		  ":checked")) { 
-			//alert("vout Check box is Checked"); 
-			selectedText = selectedText + "vout" + " " + $("#voutArtefact").val() + "," ;
-		} else { 
-			//alert("vout Check box is Unchecked"); 
-		}
-		
-		if ($("#vin").is( 
-		  ":checked")) { 
-			//alert("vin Check box is Checked");
-			selectedText = selectedText + "vin" + " " + $("#vinArtefact").val() + "," ; 
-		} else { 
-			//alert("vin Check box is Unchecked"); 
-		} 
-		//checkbox selection done. Now see dropdown selection.
-		/*$("#vddArtefact").change(function() {
-			alert( $('option:selected', this).text() );
-		});*/
-		
-		//document.getElementById("demo").innerHTML = "You selected: " + selectedText + ". Populating data based on the above selection" ;
 		$("#data").css("display","block");
-		//document.getElementById("data").style.display = "visible";
-		
 	}); 
 }); 
-
-$(document).ready(function() { 
+/*$(document).ready(function() { 
 	$(".coverage_object").change(function () {
         var end = this.value;
         $("#coverage_type").empty();
@@ -54,12 +19,32 @@ $(document).ready(function() {
 			alert("Please select an option");
     });
 });
+*/
+function populate(obj){
+	//alert(obj.value + " " +obj.id);
+	var type = obj.id.split('_');
+	//alert(type[type.length-1]);
+	var cov_type_id = document.getElementById("coverage_type_" + type[type.length-1]);
+	var val = obj.value;
+	if(val.indexOf("cg1") != -1){
+		let cg = {'-':'Select','range':'Range','level':'Level','deglitch':'Deglitch'};
+		for(let c in cg){$(cov_type_id).append($('<option>',{value: c,text: cg[c]}))}	
+		console.log(val.indexOf('cg1'));
+	}else if(val.indexOf("cg2") != -1){
+		let cg = {'-':'Select','level':'Level','ddt':'Ddt'};
+		for(let c in cg){$(cov_type_id).append($('<option>',{value: c,text: cg[c]}))}
+		console.log(val.indexOf('cg2'));
+	}else
+		alert("Please select an option");
+}
 
 $(document).ready(function() { 
 	$("#add").click(function() {
 		
-		var div = "<div id='child'> <select class='coverage_object'> <option value='-'>Select</option> <option value = 'vout_cg1'> Vout_cg1 </option> <option value = 'vdd_cg1'> vdd_cg1 </option> <option value = 'vout_cg2'> vout_cg2 </option> <option value = 'vin_cg2'> vin_cg2 </option> </select> <select id='coverage_type' > <option value='-'>Select</option> </select> <br> </div> " ;
-		
+		/*var div = "<div id='child'> <select class='coverage_object'> <option value='-'>Select</option> <option value = 'vout_cg1'> Vout_cg1 </option> <option value = 'vdd_cg1'> vdd_cg1 </option> <option value = 'vout_cg2'> vout_cg2 </option> <option value = 'vin_cg2'> vin_cg2 </option> </select> <select id='coverage_type' > <option value='-'>Select</option> </select> <br> </div> " ;
+		*/
+		var div = "<div id='child'> <select id=coverage_object_"+global_cnt+" onchange='populate(this)'> <option value='-'>Select</option> <option value = 'vout_cg1'> Vout_cg1 </option> <option value = 'vdd_cg1'> vdd_cg1 </option> <option value = 'vout_cg2'> vout_cg2 </option> <option value = 'vin_cg2'> vin_cg2 </option> </select> <select id=coverage_type_"+global_cnt +" > </select> <br> </div>  ";
+		global_cnt++;
 		$("#top_container").append(div);
 	});
 });
